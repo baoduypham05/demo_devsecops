70f0538b24b7958263f65fd13423318760db76b4
Bao Duy Pham <phambaod4@gmail.com>
2026-06-16 14:57:43 +0700

    fix: dung dinh dang aws secret chuan de kich hoat lỗi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 # Đây là lỗi Hardcoded Secret (Lộ mã bí mật) cực kỳ nguy hiểm
 # Công cụ quét SAST chắc chắn sẽ nhảy dựng lên và bắt lỗi này!
-AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE-SUPER-SECRET-KEY"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 def main():
     print("Ứng dụng đang khởi chạy...")
